f9d6c4d8840d13720fb4fc11fee16bc16d4288d4
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2019-04-25 00:19:40 -0500

    Función para hacer peticiones anonimas desde anonimizacionPrueba

--- reportePrueba.py
+++ reportePrueba.py
@@ -27,11 +27,31 @@ def printLinks(links):
 def getResults(search, links):
 	if 'filetype' in search:
 		printLinks(getLinksFiletype(links, search[search.find('filetype:'):]))
+
+def busquedaReporte(search):
+	# search_engines = ['Google', 'Bing', 'Baidu', 'Yahoo']
+	search_engines = ['DuckDuckGo']
+	for search_engine in search_engines:
+		query = busqueda.search_results(search, search_engine, 50)[1]
+		soup = beautifulSoup(query)
+		print search_engine
+		#print soup
+		if search_engine in ['Google', 'Bing']:
+			links = getLinks([ href.get('href') for href in soup.findAll('a') ]) # Google
+			getResults(search, links)
+		elif search_engine == 'Yahoo':
+			links = [ href.find('a', href=True)['href'] for href in soup.find('div', id='web').findAll('h3') ]
+			links_yahoo = [ urllib.unquote_plus(i)[i.find('http'):] for i in str(links).split('/') if i.startswith('RU') ]
+			getResults(search,links_yahoo)
+		elif search_engine == 'Baidu':
+			links = getLinks([ href.find('a', href=True)['href'] for href in soup.findAll('div', {'class': 'c-container'}) ])  # baidu
+			printLinks(links)
+
 if __name__ == '__main__':
 	search = 'filetype:pdf unam site:unam.mx'
 	#search = 'ip:192.168'
-	search_engines = ['Google', 'Bing', 'Baidu', 'Yahoo']
-	#search_engines = ['DuckDuckGo']
+	#search_engines = ['Google', 'Bing', 'Baidu', 'Yahoo']
+	search_engines = ['DuckDuckGo']
 	for search_engine in search_engines:
 		query = busqueda.search_results(search, search_engine, 50)[1]
 		soup = beautifulSoup(query)
