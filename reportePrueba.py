2de3cdc6ecfb8461242a23ab3d058cad827300db
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2019-04-24 23:01:40 -0500

    Resultados de tipo de archivo (Google,Yahoo,Bing,Baidu)

--- reportePrueba.py
+++ reportePrueba.py
@@ -5,28 +5,45 @@ import busquedaPrueba as busqueda
 
 import bs4
 import re
+import urllib
 
-def beautifulSoup(query_search_engine):
-	return 	bs4.BeautifulSoup(query_search_engine, 'lxml')
+def beautifulSoup(query_search_engine, parser = 'lxml'):
+	return 	bs4.BeautifulSoup(query_search_engine, parser)
 
 def getLinks(lista):
 	return filter(None, [ None if lista[i] is not None and (lista[i] is '#' or lista[i].startswith('/')) else lista[i] for i in range(0,len(lista)) ])
 
-def getLinksFiletype(lista, filetype):
-	return filter(None, [ lista[i] if re.search(r'%s$' % filetype, lista[i]) else None for i in range(0,len(lista)) ])
+def getLinksFiletype(lista, search):
+	operacion=re.match(r'(filetype):(.+)($| (.*))',search)
+	print operacion.groups()
+	filetype = operacion.group(2).split()[0]
+	print filetype
+	return [ lista[i] for i in range(0,len(lista)) if re.search(r'%s$' % filetype, lista[i]) ]
 
 def printLinks(links):
 	for link in links:
 		print link
 
+def getResults(search, links):
+	if 'filetype' in search:
+		printLinks(getLinksFiletype(links, search[search.find('filetype:'):]))
 if __name__ == '__main__':
 	search = 'filetype:pdf unam site:unam.mx'
-	### Por ahora solamente probado con Google
-	query_google = busqueda.search_results(search, 'Google', 50)[1]
-	soup_google = beautifulSoup(query_google)
-	#print soup_google.prettify().encode('utf-8')  # se formatea en html para poder obtener lo que se requiera con soup
-	links_google = getLinks([ href.get('href') for href in soup_google.findAll('a') ])
-	#printLinks(links_google)  # obtiene todos los enlaces encontrados de la consulta
-	if 'filetype' in search:
-		filetype = search[search.find('filetype:')+9:search.find(' ',search.find('filetype:'))]
-		printLinks(getLinksFiletype(links_google, filetype))
+	#search = 'ip:192.168'
+	search_engines = ['Google', 'Bing', 'Baidu', 'Yahoo']
+	#search_engines = ['DuckDuckGo']
+	for search_engine in search_engines:
+		query = busqueda.search_results(search, search_engine, 50)[1]
+		soup = beautifulSoup(query)
+		print search_engine
+		#print soup
+		if search_engine in ['Google', 'Bing']:
+			links = getLinks([ href.get('href') for href in soup.findAll('a') ]) # Google
+			getResults(search, links)
+		elif search_engine == 'Yahoo':
+			links = [ href.find('a', href=True)['href'] for href in soup.find('div', id='web').findAll('h3') ]
+			links_yahoo = [ urllib.unquote_plus(i)[i.find('http'):] for i in str(links).split('/') if i.startswith('RU') ]
+			getResults(search,links_yahoo)
+		elif search_engine == 'Baidu':
+			links = getLinks([ href.find('a', href=True)['href'] for href in soup.findAll('div', {'class': 'c-container'}) ])  # baidu
+			printLinks(links)
